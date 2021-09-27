09491be62e4c1a2dff7da255bf03d037f1645f3b
Cristian Camilo Alarcon Medina <80474928+cristiancamilo03@users.noreply.github.com>
2021-09-27 15:47:32 -0500

    release/Cristian03-implements-api-json-serve

--- src/MainMenu.jsx
+++ src/MainMenu.jsx
@@ -2,25 +2,12 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const MainMenu = () => (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Inazuma Eleven App</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-      <a class="nav-link"><NavLink exact to="/">Home</NavLink></a>
-      </li>
-      <li class="nav-item">
-      <a class="nav-link"><NavLink exact to="/personajes">Personajes</NavLink></a>
-      </li>
-      <li class="nav-item">
-      <a class="nav-link"><NavLink exact to="/contacto">Contacto</NavLink></a>
-      </li>
+    <ul>
+        <li><NavLink exact to="/">Home</NavLink></li>
+        <li><NavLink exact to="/estudiantes">Estatudiantes</NavLink></li>
+        <li><NavLink exact to="/contacto">Contacto</NavLink></li>
+        <li><NavLink exact to="/datos_api">Datos API</NavLink></li>
     </ul>
-  </div>
-</nav>
 )
 
 export default MainMenu;
